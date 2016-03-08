c6a47cc4497a661297aece0bc1aa3cf6e0bd7f2e
Ben Hamner <ben@benhamner.com>
2016-03-08 11:47:37 -0800

    updated created readme.txt

--- src/readme.py
+++ src/readme.py
@@ -1,6 +1,4 @@
-readme = """
-
-This data release lives at https://kaggle.com/seanlahman/baseball and was created by code at https://github.com/benhamner/baseball.
+readme = """This data release lives at https://kaggle.com/seanlahman/baseball and was created by code at https://github.com/benhamner/baseball.
 
 It is a processed version of the 2015 data at www.seanlahman.com/baseball-archive/statistics/.
 
@@ -13,7 +11,7 @@ The two key modifications from the original database were:
  - we updated column names to have consistent formatting and naming
  - we created a SQLite datbase in addition to the original CSV files
 
-Below, we're copying README.txt and readme2014.txt from the original 2015 database. Note that the column names may have been modified from the ones stated in readme2014.txt. 
+Below, we're copying README.txt and readme2014.txt from the original 2015 database. Note that the column names may have been modified from the ones stated in readme2014.txt. See https://github.com/benhamner/baseball/blob/master/src/process.py#L43 for the modifications to column names.
 
 README.txt
 ==============================================================
